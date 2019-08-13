39b84e109bc1cb73504f2a6134cfb387f572d58a
Connor <24Headcrusher24@gmail.com>
2019-08-13 15:53:18 +1200

    Fixed testing error

--- src/test/java/seng202/teamsix/AppTest.java
+++ src/test/java/seng202/teamsix/AppTest.java
@@ -1,69 +1,59 @@
 package seng202.teamsix;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
         assertTrue( true );
     }
 
+    @Test
     public void testFoo() {
         App app = new App();
         assertTrue(app.foo());
     }
 
+    @Test
     public void testFooTaran() {
         assertTrue(1+2 == 3);
     }
 
+    @Test
     public void testQuickMath() {
         System.out.println("goes through this test case");
         assertTrue(2+2 == 4);
     }
 
+    @Test
     public void testFooAnzac() {
         System.out.println("Testing");
         assertTrue(13 * 2 == 26);
     }
 
+    @Test
     public void testAndy() {
         assertEquals(1 + 2, 3);
     }
 
+    @Test
     public void testRchi() {
-        Systemout.println("BLAHHH");
+        System.out.println("BLAHHH");
         assertEquals(2 + 2, 4);
     }
 
+    @Test
     public void testForGeorge() {
         System.out.println("Testing for george");
         assertEquals(4+3, 7);
