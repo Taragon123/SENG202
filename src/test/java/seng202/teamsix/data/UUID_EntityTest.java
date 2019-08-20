ed5976dac60ec8e05574c1a30f6a37a1fe72c988
Connor <24Headcrusher24@gmail.com>
2019-08-20 13:25:39 +1200

    Added UUID_Entity test cases

--- src/test/java/seng202/teamsix/data/UUID_EntityTest.java
+++ src/test/java/seng202/teamsix/data/UUID_EntityTest.java
@@ -1,12 +1,54 @@
 package seng202.teamsix.data;
 
+
 import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 import static org.junit.Assert.*;
 
 public class UUID_EntityTest {
 
     @Test
-    public void generateUUID() {
+    public void testGenerateUUID() {
+        // If this function ever fails everyone buy lotto tickets.
+
+        UUID_Entity entity = new UUID_Entity();
+        Set<UUID> generated_uuids = new HashSet<UUID>();
+
+        for(int i = 0; i < 1000; i++) {
+            entity.generateUUID();
+
+            assertFalse(generated_uuids.contains(entity.getUUID()));
+            generated_uuids.add(entity.getUUID());
+        }
+    }
+
+    @Test
+    public void testSetUUID() {
+        long MSB = 0x0000DEADBEEF0000L;
+        long LSB = 0x1111FEEDC0DE1111L;
+
+        UUID_Entity entity = new UUID_Entity();
+        entity.setUUID(MSB, LSB);
+        assertEquals(entity.getUUID().getMostSignificantBits(), MSB);
+        assertEquals(entity.getUUID().getLeastSignificantBits(), LSB);
+    }
+
+    @Test
+    public void testEquals() {
+        UUID_Entity entity = new UUID_Entity();
+        entity.generateUUID();
+
+        UUID_Entity same_entity = new UUID_Entity();
+        same_entity.setUUID(entity.getUUID().getMostSignificantBits(), entity.getUUID().getLeastSignificantBits());
+
+        UUID_Entity different_entity = new UUID_Entity();
+        different_entity.generateUUID();
+
+        assertTrue(entity.equals(same_entity));
+        assertFalse(entity.equals(different_entity));
     }
 }
