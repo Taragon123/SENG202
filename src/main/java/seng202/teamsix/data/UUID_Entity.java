a3d7d7b68b84e7e96db8768228f5d0a02e77219f
Connor <24Headcrusher24@gmail.com>
2019-08-20 13:25:04 +1200

    Added equals method to UUID_Entity

--- src/main/java/seng202/teamsix/data/UUID_Entity.java
+++ src/main/java/seng202/teamsix/data/UUID_Entity.java
@@ -1,4 +1,46 @@
 package seng202.teamsix.data;
 
+import java.util.UUID;
+
 public class UUID_Entity {
+    // Members
+    private UUID uuid;
+
+
+    // Methods
+
+    /**
+     * Sets internal uuid
+     * @param msb most significant 8 bytes in uuid
+     * @param lsb least significant 8 bytes in uuid
+     */
+    public void setUUID(long msb, long lsb) {
+        uuid = new UUID(msb, lsb);
+    }
+
+    /**
+     * Returns internal uuid
+     * @return uuid
+     */
+    public UUID getUUID() {
+        return uuid;
+    }
+
+    /**
+     * Randomly generates an internal uuid
+     */
+    public void generateUUID() {
+        uuid = UUID.randomUUID();
+    }
+
+    /**
+     * Tests if two uuid entities are equal.
+     * This function should not be overwritten by subclasses as the uuid should
+     * always be the condition of equality.
+     * @param entity compare equality with
+     * @return true if equals entity
+     */
+    public boolean equals(UUID_Entity entity) {
+        return uuid.equals(entity.getUUID());
+    }
 }
---
+++ src/test/java/seng202/teamsix/data/UUID_EntityTest.java
@@ -0,0 +1,12 @@
+package seng202.teamsix.data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UUID_EntityTest {
+
+    @Test
+    public void generateUUID() {
+    }
+}
