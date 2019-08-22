16f7138e079d74f0e693758f6d159b6da6f538e4
gms122 <gms122@uclive.ac.nz>
2019-08-22 18:09:21 +1200

    Fixed missing functions in ItemTag and ItemTagTest

--- src/main/java/seng202/teamsix/data/ItemTag.java
+++ src/main/java/seng202/teamsix/data/ItemTag.java
@@ -1,4 +1,33 @@
 package seng202.teamsix.data;
 
 public class ItemTag {
+    /**
+     * Class for tags
+     */
+    //Members
+    private String name;
+    private Boolean is_dominant;
+
+    //Constructors
+    public ItemTag(String name, Boolean is_dominant) {
+        this.name = name;
+        this.is_dominant = is_dominant;
+    }
+
+    //Functions
+    /**
+     * Gets the value of is_dominant.
+     * @return Boolean true if tag is dominant
+     */
+    public Boolean getIsDominant() {
+        return is_dominant;
+    }
+
+    /**
+     * Gets the value of name.
+     * @return String name of tag
+     */
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/seng202/teamsix/data/ItemTagTest.java
+++ src/test/java/seng202/teamsix/data/ItemTagTest.java
@@ -1,5 +1,24 @@
+package seng202.teamsix.data;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ItemTagTest {
 
+    @Test
+    void getIsDominant() {
+        ItemTag tag = new ItemTag("Gluten free!", false);
+        assertEquals(false, tag.getIsDominant());
+        tag = new ItemTag("this tag is very cool my dude", true);
+        assertEquals(true, tag.getIsDominant());
+    }
+
+    @Test
+    void getName() {
+        ItemTag tag = new ItemTag("Gluten free!", false);
+        assertEquals("Gluten free!", tag.getName());
+        tag = new ItemTag("this tag is very cool my dude", false);
+        assertEquals("this tag is very cool my dude", tag.getName());
+    }
 }
