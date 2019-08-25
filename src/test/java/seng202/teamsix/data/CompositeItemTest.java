55eccc5e8ba7eacd498b38466ed3d064c73a5b62
gms122 <gms122@uclive.ac.nz>
2019-08-25 18:05:46 +1200

    Fixed build issues in CompositeItemTest.java

--- src/test/java/seng202/teamsix/data/CompositeItemTest.java
+++ src/test/java/seng202/teamsix/data/CompositeItemTest.java
@@ -1,14 +1,11 @@
 package seng202.teamsix.data;
 
 import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class CompositeItemTest {
+public class CompositeItemTest {
     @Test
-    void testGetItems() {}
+    public void testGetItems() {
+        System.out.println("In progress");
+    }
 }
