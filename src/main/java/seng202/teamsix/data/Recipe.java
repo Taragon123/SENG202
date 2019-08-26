25ef8438ee9529d58c96bce1e50366cc8da88f99
hra62 <hra62@uclive.ac.nz>
2019-08-27 11:47:52 +1200

    Added in the Item class, as well as filling in docstring and completing some tests. Recipe class seems short and wasteful though, might bring up at next meeting.

--- src/main/java/seng202/teamsix/data/Recipe.java
+++ src/main/java/seng202/teamsix/data/Recipe.java
@@ -2,10 +2,18 @@ package seng202.teamsix.data;
 
 /**
  * Class Recipe, holds the recipe as an object with a string attribute called method which is essentially the instructions
- * to making a certain item, such as a burger.
+ * to making a certain item, such as a burger. This is class because we may add more methods and attributes in future.
  */
 public class Recipe {
     private String method;
+
+    /**
+     * A getter for the method attribute.
+     * @return The recipe
+     */
+    public String getMethod() {
+        return method;
+    }
 
     /**
      * Constructor class for Recipe.
--- src/main/java/seng202/teamsix/data/UnitType.java
+++ src/main/java/seng202/teamsix/data/UnitType.java
@@ -1,4 +1,13 @@
 package seng202.teamsix.data;
+/**
+ * Name: UnitType.java
+ * Author: Hamesh Ravji
+ * Date: August, 2019
+ */
 
+/**
+ * UnitType enum used to indicate quantity units in relation to the item.
+ */
 public enum UnitType {
+    KG, G, L, ML, NUM
 }
