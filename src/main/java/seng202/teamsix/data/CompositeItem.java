7dbd8deb2e2cb2f4ee073dc2f6fb254331e149c5
gms122 <gms122@uclive.ac.nz>
2019-08-30 16:34:23 +1200

    Updated CompositeItem and CompositeItemTest

--- src/main/java/seng202/teamsix/data/CompositeItem.java
+++ src/main/java/seng202/teamsix/data/CompositeItem.java
@@ -1,20 +1,63 @@
+/**
+ * Name: CompositeItem.java
+ * Authors: George Stephenson
+ * Date: 22/08/2019
+ */
 package seng202.teamsix.data;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class CompositeItem {
-    private List<UUID_Entity> item_list;
+public class CompositeItem extends Item{
+    private List<Item> item_list;
     private Recipe recipe;
 
-    public CompositeItem(List<UUID_Entity> item_list) {
+    /**
+     * Constructor for blank composite item.
+     */
+    public CompositeItem() {
+        super("NA", "NA", new Currency(), new Currency(), new Recipe("NA"), new ArrayList<ItemTag>(), UnitType.KG);
+    }
+
+    /**
+     * Constructor that takes a list of items as parameter.
+     * @param item_list List of UUID_Entity components
+     */
+    public CompositeItem(String name, String description, Currency base_price, Currency markup_price, Recipe recipe,
+                         ArrayList<ItemTag> tags, UnitType qty_unit, List<Item> item_list) {
+        super(name, description, base_price, markup_price, recipe, tags, qty_unit);
         this.item_list = item_list;
     }
 
-    public List<UUID_Entity> getItems() {
+    /**
+     * Sets recipe for composite item.
+     * @param recipe Recipe object
+     */
+    public void setCompositeRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    /**
+     * Sets list of components.
+     * @param item_list List of UUID_Entity components
+     */
+    public void setComponents(List<Item> item_list) {
+        this.item_list = item_list;
+    }
+
+    /**
+     * Getter for component items.
+     * @return List of UUID_Entity components
+     */
+    public List<Item> getItems() {
         return item_list;
     }
 
-    public  String getRecipe() {
-        return recipe.toString();
+    /**
+     * Gets recipe for combining components.
+     * @return String explaining how to combine components
+     */
+    public String getCompositeRecipe() {
+        return recipe.getMethod();
     }
 }
--- src/test/java/seng202/teamsix/data/CompositeItemTest.java
+++ src/test/java/seng202/teamsix/data/CompositeItemTest.java
@@ -1,11 +1,38 @@
+/**
+ * Name: CompositeItemTest.java
+ * Authors: George Stephenson
+ * Date: 22/08/2019
+ */
+
 package seng202.teamsix.data;
 
 import org.junit.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class CompositeItemTest {
     @Test
-    public void testGetItems() {
-        System.out.println("In progress");
+    public void testSetGetItems() {
+        CompositeItem item = new CompositeItem();
+        ArrayList<ItemTag> tags = new ArrayList<ItemTag>();
+        ArrayList<Item> subItems = new ArrayList<Item>();
+        subItems.add(new Item("Coke", "Cold one", new Currency(), new Currency(), tags, UnitType.KG));
+        item.setComponents(subItems);
+        assertEquals(item.getItems().size(), 1);
+    }
+
+    @Test
+    public void testSetGetRecipe() {
+        CompositeItem item = new CompositeItem();
+        item.setCompositeRecipe(new Recipe("Do nothing, there's no components!"));
+        assertEquals(item.getCompositeRecipe(), "Do nothing, there's no components!");
+    }
+
+    @Test
+    public void test() {
+
     }
 }
