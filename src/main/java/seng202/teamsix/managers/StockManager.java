bc0434088e8dd47275386eebee1bffc62bcd4427
Andrew Clifford <acl118@uclive.ac.nz>
2019-09-01 18:15:21 +1200

    Updated StockManager Class, added StockChecker Class

--- src/main/java/seng202/teamsix/managers/StockManager.java
+++ src/main/java/seng202/teamsix/managers/StockManager.java
@@ -7,13 +7,13 @@ import seng202.teamsix.data.StockInstance;
  * Class StockManager, used to manage the current stock inventory
  * Author: Andrew Clifford
  * Date Created: 31/08/19
- * Last Modified: 31/08/19
+ * Last Modified: 01/09/19
  */
 public class StockManager {
 
     private ArrayList<StockInstance> stock_list;
 
-    /* Constructors below, unsure of how they are going to be instantiated atm */
+    /* Constructors below, not 100% sure at this stage how the StockManager is going to be instantiated */
 
     /**
      * Constructor for the StockManager class, used if there is no current list of stock items
@@ -31,22 +31,35 @@ public class StockManager {
     }
 
     /**
-     * Adds the given StockInstance to the ArrayList of StockInstances
-     * @param instance StockInstance to be added to the list of StockInstances
+     * Adds the given StockInstance to the ArrayList stock_items
+     * @param instance StockInstance to be added to stock_items
+     * @return boolean, true if the StockInstance was successfully added to stock_items, false otherwise
      */
-    public void addStockInstance(StockInstance instance) {
+    public boolean addStock(StockInstance instance) {
+        boolean bool;
         if (!stock_list.contains(instance)) {
             stock_list.add(instance);
+            return true;
         } else {
             System.err.println("Can't add same StockInstance to the list of stock_items!");
+            return false;
         }
     }
 
-    public void removeStockInstance(StockInstance instance) {
+    /**
+     * Removes the given StockInstance from the ArrayList stock_items
+     * @param instance StockInstance to be removed from stock_items
+     * @return boolean, true if the StockInstance was successfully removed from stock_items, false otherwise
+     */
+    public boolean removeStock(StockInstance instance) {
         if (stock_list.contains(instance)) {
             stock_list.remove(instance);
+            return true;
         } else {
             System.err.println("The specified StockInstance is not in the list of stock_items!");
+            return false;
         }
     }
+
+    public ArrayList<StockInstance> getStockList() { return stock_list; }
 }
