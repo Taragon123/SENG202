e21bf78f987876a6efe6f313d8fe4d724a08c4fb
Andrew Clifford <acl118@uclive.ac.nz>
2019-09-05 14:44:45 +1200

    Created StockManagerTest Class

--- src/test/java/seng202/teamsix/managers/StockManagerTest.java
+++ src/test/java/seng202/teamsix/managers/StockManagerTest.java
@@ -1,7 +1,14 @@
 package seng202.teamsix.managers;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StockManagerTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
 
 }
