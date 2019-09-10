95d66371e528ebaac154a07ae175bf0cddaa1eaa
apm108 <apm108@uclive.ac.nz>
2019-09-10 18:44:16 +1200

    First cucumber style test officially complete (took way longer than I thought), setter class added to the cash register class (needed to get the test working)

--- src/test/java/seng202/teamsix/data/AcceptanceTestStepDefs.java
+++ src/test/java/seng202/teamsix/data/AcceptanceTestStepDefs.java
@@ -1,22 +1,28 @@
 package seng202.teamsix.data;
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AcceptanceTestStepDefs {
     CashRegister cashRegister;
+
     @Given("${int} current total cash")
     public void $CurrentTotalCash(int arg0) {
         cashRegister = new CashRegister();
-        //cashRegister.setDollars(arg0);
+
+        cashRegister.setRegisterAmount(10000);
     }
 
     @When("${int} is add to the current total cash")
     public void $IsAddToTheCurrentTotalCash(int arg0) {
+        cashRegister.setRegisterAmount(5000);
     }
 
     @Then("The current total cash is now ${int}")
     public void theCurrentTotalCashIsNow$(int arg0) {
+        assertEquals(150, cashRegister.getRegisterAmount());
     }
 }
