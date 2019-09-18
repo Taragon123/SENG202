6664e46d6a74710083019c1317bb004eadf5723f
gms122 <gms122@uclive.ac.nz>
2019-09-18 14:16:03 +1200

    Added groundwork for dynamic button creation in StockScreenController

--- src/main/java/seng202/teamsix/GUI/StockScreenController.java
+++ src/main/java/seng202/teamsix/GUI/StockScreenController.java
@@ -2,10 +2,7 @@ package seng202.teamsix.GUI;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import seng202.teamsix.data.StockInstance;
@@ -18,19 +15,43 @@ import java.util.ResourceBundle;
 
 
 public class StockScreenController implements Initializable {
-    private ArrayList<StockInstance> stocks = new ArrayList<StockInstance>();
+    private int[] stocks = {1, 3, 4};
+
 
     @FXML
     private GridPane itemPane;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 
+    public GridPane createItemDisplay(int i) {
+        GridPane grid = new GridPane();
+        Label name = new Label("Hello there");
+        Button editBtn = new Button(String.valueOf(stocks[i]));
+        grid.add(editBtn, 0, 0);
+        grid.add(name, 1, 0);
+        return grid;
+    }
 
+    public void populateList() {
+        itemPane.getChildren().clear();
+        for (int i = 0; i < stocks.length; i++) {
+            GridPane grid = createItemDisplay(i);
+            itemPane.add(grid, 0, i);
+        }
+    }
+
+    public void refreshScrollPane() {}
+
+    public void addStock() {}
+
+    public void updateStock() {}
 
     public void searchItems() {
-        GridPane data = new ItemTableData().getGrid();
-        itemPane.add(data, 0, 0);
+        populateList();
+//        GridPane data = new ItemTableData().getGrid();
+//        itemPane.add(data, 0, 0);
     }
 }
